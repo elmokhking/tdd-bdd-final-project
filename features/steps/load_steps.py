4d80aecd441a42124bbd3d0c7e7853a609a0e101
elmokhking <enourhira@gmail.com>
2024-03-17 13:39:28 -0400

    exercice 5

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -42,11 +42,16 @@ def step_impl(context):
     for product in context.resp.json():
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
-
     #
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name":row["name"],
+            "description":row["description"],
+            "price":row["price"],
+            "available":row["available"]  in ['True', 'true', '1'],
+            "category":row["category"]
+        }
+        response = requests.post(rest_endpoint, json=payload)
+        assert(response.status_code == HTTP_201_CREATED)
